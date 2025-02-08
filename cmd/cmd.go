5d139bc260fd1183228a312b76c998906d5cbb9c
Amin Rezaei <amin.zdev@gmail.com>
2025-02-09 00:56:15 +0330

    Update cmd.go

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -4,9 +4,14 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var (
+	output   string
+	password string
+)
+
 // RootCmd is the main command that sets up the basic CLI structure and command handling.
 var RootCmd = &cobra.Command{
-	Use:   "backup-manager",
+	Use:   "zsafe",
 	Short: "A simple backup manager CLI for creating and restoring backups",
 	Long: `Backup Manager allows you to create and restore backups of files or directories.
 You can compress and encrypt backups for security and restore them later.`,
--- cmd/cmd_test.go
+++ cmd/cmd_test.go
@@ -25,40 +25,6 @@ func TestRootCmd(t *testing.T) {
 	assert.Contains(t, buf.String(), "Please specify a command")
 }
 
-// TestBackupCmd tests the 'backup' command
-func TestBackupCmd(t *testing.T) {
-	// Create a buffer to capture the output of the command
-	var buf strings.Builder
-	RootCmd.SetOut(&buf)
-
-	// Run the 'backup' command without any arguments
-	RootCmd.SetArgs([]string{"backup"})
-	err := RootCmd.Execute()
-
-	// Assert that there is no error in executing the command
-	assert.NoError(t, err)
-
-	// Assert that the output contains the expected text
-	assert.Contains(t, buf.String(), "Creating backup...")
-}
-
-// TestRestoreCmd tests the 'restore' command
-func TestRestoreCmd(t *testing.T) {
-	// Create a buffer to capture the output of the command
-	var buf strings.Builder
-	RootCmd.SetOut(&buf)
-
-	// Run the 'restore' command without any arguments
-	RootCmd.SetArgs([]string{"restore"})
-	err := RootCmd.Execute()
-
-	// Assert that there is no error in executing the command
-	assert.NoError(t, err)
-
-	// Assert that the output contains the expected text
-	assert.Contains(t, buf.String(), "Restoring backup...")
-}
-
 // TestInvalidCommand tests the invalid command behavior (e.g., an unrecognized command)
 func TestInvalidCommand(t *testing.T) {
 	// Run an invalid command
